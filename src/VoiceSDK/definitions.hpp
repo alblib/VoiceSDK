d5fdbb00fdfabf1a5ee6d04d72ef2cb8486664ce
Woohyun Rim <rim.woohyun@illuni.com>
2024-04-19 21:53:58 +0900

    c_api readying..

--- src/VoiceSDK/definitions.hpp
+++ src/VoiceSDK/definitions.hpp
@@ -7,4 +7,12 @@
 #define IFCONSTEXPR if
 #endif
 
+#ifndef VoiceSDK_EXPORTS
+# if (defined _WIN32 || defined WINCE || defined __CYGWIN__) && defined(CVAPI_EXPORTS)
+#   define VoiceSDK_EXPORTS __declspec(dllexport)
+# elif defined __GNUC__ && __GNUC__ >= 4 && (defined(CVAPI_EXPORTS) || defined(__APPLE__))
+#   define VoiceSDK_EXPORTS __attribute__ ((visibility ("default")))
+# endif
+#endif
+
 #endif
