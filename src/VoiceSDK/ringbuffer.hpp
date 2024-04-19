8f697201f7f5b257a898e2b790109743110a9cfc
Albertus Liberius <w890702@gmail.com>
2024-04-20 05:19:28 +0900

    type_traits fixed

--- src/VoiceSDK/ringbuffer.hpp
+++ src/VoiceSDK/ringbuffer.hpp
@@ -17,29 +17,29 @@ namespace VoiceSDK{
 
 #pragma region is_contiguous_iterator
 
-template <class Iter>
-struct is_contiguous_iterator : std::integral_constant<bool, false> {};
+template <class Iter, typename = void>
+struct is_contiguous_iterator : std::false_type {};
 
-template <class Ptr, typename = std::enable_if<std::is_pointer<Ptr>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
+template <class Ptr>
+struct is_contiguous_iterator<Ptr, typename std::enable_if<std::is_pointer<Ptr>::value>::type> : std::true_type {};
 
 #if _CPP_VER >= 202002L
-template <class Iter, typename = std::enable_if_t<std::contiguous_iterator<Iter>>>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, std::enable_if_t<std::contiguous_iterator<Iter>>> : std::true_type {};
 #else
 
-template <class Iter, typename = std::enable_if<std::is_same<Iter, typename std::vector<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
-template <class Iter, typename = std::enable_if<std::is_same<Iter, typename std::array<typename std::iterator_traits<Iter>::value_type, 10>::iterator>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
-template <class Iter, typename = std::enable_if<std::is_same<Iter, decltype(std::begin(std::declval<std::valarray<typename std::iterator_traits<Iter>::value_type>>()))>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
-template <class Iter, typename = std::enable_if<std::is_same<Iter, typename std::basic_string<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, typename std::enable_if<std::is_same<Iter, typename std::vector<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type> : std::true_type {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, typename std::enable_if<std::is_same<Iter, typename std::array<typename std::iterator_traits<Iter>::value_type, 10>::iterator>::value>::type> : std::true_type {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, typename std::enable_if<std::is_same<Iter, decltype(std::begin(std::declval<std::valarray<typename std::iterator_traits<Iter>::value_type>>()))>::value>::type> : std::true_type {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, typename std::enable_if<std::is_same<Iter, typename std::basic_string<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type> : std::true_type {};
 
 #if _CPP_VER >= 201703L
-template <class Iter, typename = std::enable_if<std::is_same<Iter, typename std::basic_string_view<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type>
-struct is_contiguous_iterator : std::integral_constant<bool, true> {};
+template <class Iter>
+struct is_contiguous_iterator<Iter, typename std::enable_if<std::is_same<Iter, typename std::basic_string_view<typename std::iterator_traits<Iter>::value_type>::iterator>::value>::type> : std::true_type {};
 #endif
 
 #endif
@@ -52,15 +52,13 @@ inline constexpr bool is_contiguous_iterator_v = is_contiguous_iterator<Iter>::v
 
 #pragma region is_input_iterator
 
-template <class Iter>
-struct is_input_iterator : std::integral_constant<bool, false> {};
 
 #if _CPP_VER >= 202002L
-template <class Iter, typename = std::enable_if_t<std::input_iterator<Iter>>>
-struct is_input_iterator : std::integral_constant<bool, true> {};
+template <class Iter>
+struct is_input_iterator : std::integral_constant<bool, std::input_iterator<Iter>> {};
 #else
-template <class InputIt, typename = std::enable_if<std::is_base_of<std::input_iterator_tag, typename std::iterator_traits<InputIt>::iterator_category>::value>::type>
-struct is_input_iterator : std::integral_constant<bool, true> {};
+template <class Iter>
+struct is_input_iterator : std::integral_constant<bool, std::is_base_of<std::input_iterator_tag, typename std::iterator_traits<Iter>::iterator_category>::value> {};
 #endif
 
 template <class Iter>
